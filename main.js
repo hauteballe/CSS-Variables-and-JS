67ea12002f5c9da5db645f6ea43eb8f6d0f798da
Irina Sinitsyna <hauteballe@gmail.com>
2020-12-10 17:31:16 +0300

    feat: implement CSS Variables and JS

---
+++ main.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate(event) {
+  const suffix = event.target.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${event.target.name}`,
+    event.target.value + suffix
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
